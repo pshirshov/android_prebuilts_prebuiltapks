1a1945d5786661eeffc9bb1093c39ce1c9fa233f
Pavel Shirshov <pshirshov@eml.cc>
2021-09-11 18:50:03 +0100

    fixes

--- KdeConnect/Android.mk
+++ KdeConnect/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := privapp-permissions-org.kde.kdeconnect.MyApplication.xml
+LOCAL_MODULE := privapp-permissions-org.kde.kdeconnect_tp.xml
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
@@ -16,6 +16,6 @@ LOCAL_MODULE_CLASS := APPS
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_REQUIRED_MODULES := privapp-permissions-org.kde.kdeconnect.MyApplication.xml
+LOCAL_REQUIRED_MODULES := privapp-permissions-org.kde.kdeconnect_tp.xml
 include $(BUILD_PREBUILT)
 
